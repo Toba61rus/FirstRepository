bb9954b241cd7fb3c4dfe70c9a5d7012c8f9716b
Toba61rus <toba92@mail.ru>
2018-10-29 22:53:23 +1000

    Решённая задача 8. 7ую задачу закоментил,т.к. решения не нашёл.

---
+++ src/ru/dvfu/tabyka/tasks01/Main08.java
@@ -0,0 +1,13 @@
+package ru.dvfu.tabyka.tasks01;
+import java.util.Scanner;
+
+public class Main08 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(" Пожалуйста, введите количество рублей, которое хотели бы перевести в доллары и евро ");
+        double g = s.nextDouble();
+        double x = g /65.56;
+        double y = g /74.67;
+        System.out.println( g +  " рублей равно " + x +" долларам " + " и " + y+  " евро по курсу на 29.10.2018 г." );
+    }
+}
