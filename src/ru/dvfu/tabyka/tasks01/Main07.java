2e58c90885d20c6bb5620a0ea2a74525c2c0dd63
Toba61rus <toba92@mail.ru>
2018-11-02 12:34:58 +1000

    Решённая 7ая задача.

--- src/ru/dvfu/tabyka/tasks01/Main07.java
+++ src/ru/dvfu/tabyka/tasks01/Main07.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main07 {
     public static void main(String[] args) {
 final double pi = 3.14;
-       double l = 2 *pi * r ;// Формула нахождения длины окружности L=pi *D = 2 * pi * r.
+       // Формула нахождения длины окружности L=pi *D = 2 * pi * r.
         System.out.println(" Данная программа находит длину и площадь окружности, указав радиус ");
 Scanner s = new Scanner(System.in);
         System.out.println(" Укажите радиус " );
 double r = s.nextDouble();
-        System.out.println(" При радиусе " + r + "длина окружности равна " + l);
+        System.out.println(" При радиусе " + r + "длина окружности равна " + 2 * pi * r );
         System.out.println(" Площадь круга составляет " + pi * Math.pow(r , 2));
     }
 }
